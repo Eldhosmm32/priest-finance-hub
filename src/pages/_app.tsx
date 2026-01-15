805ce6d412a457c5673004c1d570831a311a5845
Eldhose Mathai <eldhose.mathai@vantlogix.com>
2026-01-15 14:34:05 +0530

    sacale meta added as header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/styles.scss";
 import { useRouter } from "next/router";
@@ -12,32 +13,31 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const isAdmin = router.pathname.startsWith("/admin");
   const isPriest = router.pathname.startsWith("/priest");
 
-  if (isAdmin) {
-    return (
-      <LanguageProvider>
-        <AdminLayout>
-          <Toaster />
-          <Component {...pageProps} />
-        </AdminLayout>
-      </LanguageProvider>
-    );
-  }
-
-  if (isPriest) {
-    return (
-      <LanguageProvider>
-        <PriestLayout>
-          <Toaster />
-          <Component {...pageProps} />
-        </PriestLayout>
-      </LanguageProvider>
-    );
-  }
-
   return (
-    <LanguageProvider>
-      <Toaster />
-      <Component {...pageProps} />
-    </LanguageProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
+      {isAdmin ? (
+        <LanguageProvider>
+          <AdminLayout>
+            <Toaster />
+            <Component {...pageProps} />
+          </AdminLayout>
+        </LanguageProvider>
+      ) : isPriest ? (
+        <LanguageProvider>
+          <PriestLayout>
+            <Toaster />
+            <Component {...pageProps} />
+          </PriestLayout>
+        </LanguageProvider>
+      ) : (
+        <LanguageProvider>
+          <Toaster />
+          <Component {...pageProps} />
+        </LanguageProvider>
+      )}
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <Head />
       <body className="bg-[url('/bg-mobile.png')] md:bg-[url('/bg.png')] bg-cover bg-center">
         <Main />
